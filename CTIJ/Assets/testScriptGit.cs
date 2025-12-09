8e0518da3b1cf8181dafe326729926b22c961cf9
AlinLemnaru <alinl.gabriel200@gmail.com>
2025-12-09 22:01:32 +0200

    git test 2

---
+++ CTIJ/Assets/testScriptGit.cs
@@ -0,0 +1,18 @@
+using System;
+using UnityEngine;
+
+public class testScriptGit : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        // Testing GIT integration
+        Debug.Log("Test GIT!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
