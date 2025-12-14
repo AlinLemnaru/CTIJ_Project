50a7d27f72eee6b68b5097a04b38e1c88631e367
adrdeatcu <adideatcu340@gmail.com>
2025-12-14 20:20:09 +0200

    create saw animation

--- CTIJ/Assets/Scripts/Health.cs
+++ CTIJ/Assets/Scripts/Health.cs
@@ -4,29 +4,31 @@ public class Health : MonoBehaviour
 {
     [SerializeField] private float startingHealth;
     public float currentHealth { get; private set; }
+    private Animator anim;
+    private bool dead;
 
     private void Awake()
     {
         currentHealth = startingHealth;
+        anim = GetComponent<Animator>();
     }
     public void TakeDamage(float damageAmount)
     {
         currentHealth = Mathf.Clamp(currentHealth - damageAmount, 0, startingHealth);
         if (currentHealth > 0)
         {
-            //player hurt
+            anim.SetTrigger("hurt");
         }
         else
         {
-            //player dead
+            if(!dead)
+            {
+            anim.SetTrigger("die");
+            GetComponent<PlayerController>().enabled = false;
+            dead = true;
+            }
         }
     }
 
-    private void Update()
-    {
-        if(Input.GetKeyDown(KeyCode.E))
-        {
-            TakeDamage(1);
-        }
-    }
+   
 }
